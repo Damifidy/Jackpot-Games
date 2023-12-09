64c68729d026df0de2040f6b25395f9214273e73
Damifidy <82963489+Damifidy@users.noreply.github.com>
2023-12-09 22:25:08 +0000

    Pending changes exported from your codespace

---
+++ script.js
@@ -0,0 +1,13 @@
+function launchab() {
+    const tab = window.open('about:blank', '_blank');
+    const iframe = tab.document.createElement('iframe');
+    const stl = iframe.style;
+    stl.border = stl.outline = 'none';
+    stl.width = '100vw';
+    stl.height = '100vh';
+    stl.position = 'fixed';
+    stl.left = stl.right = stl.top = stl.bottom = '0';
+    iframe.src = self.location;
+    tab.document.body.appendChild(iframe);
+    window.parent.window.location.replace(localStorage.getItem("panicurl") || 'https://gibdist.schoology.com/home#/')
+}
