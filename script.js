bd97a5b9528ea76d6bfe56d3f701beadfa50ec60
Damifidy <damifidy.mail@gmail.com>
2024-03-24 20:50:31 -0400

    modified:   index.html 	modified:   script.js 	modified:   script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let pw = window.prompt('Password:', '<type here>')
+if (pw !== 'C:\Users\damie\pass4JG.txt') {
+    window.parent.window.location.replace('https://google.com/')
+} else { return 'Loaded Page Successfully!' }
+
 function launchab() {
     const tab = window.open('about:blank', '_blank');
     const iframe = tab.document.createElement('iframe');
